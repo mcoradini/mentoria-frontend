f07caaa66aa29ce7439d73c26c9325b0d0957ea7
Marco Aurélio de Moraes Coradini <marco.coradini@dbserver.com.br>
2020-05-14 11:06:38 -0300

    Incluido novo teste case de login.

---
+++ src/test/java/appobjects/InventoryAppObjects.java
@@ -0,0 +1,21 @@
+package appobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class InventoryAppObjects {
+
+    private WebDriverWait wait;
+
+    public InventoryAppObjects(WebDriver driver) {
+        wait = new WebDriverWait(driver, 20);
+    }
+
+    public WebElement getShoppingCartButton() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("shopping_cart_container")));
+    }
+
+}
