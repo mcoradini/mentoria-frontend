0a07685aa7b74b172c6352c2c67628f2e61b747c
Marco Aurélio de Moraes Coradini <marco.coradini@dbserver.com.br>
2020-05-14 09:51:01 -0300

    Inclusão de classe DriverFactory e atualização de test cases utilizando para utilizar a factory.

--- src/test/java/support/TestBase.java
+++ src/test/java/support/TestBase.java
@@ -1,30 +1,20 @@
 package support;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 
-public class TestBase {
+public class TestBase extends DriverManagerFactory {
 
-    public WebDriver driver;
+    private WebDriver driver;
 
-    @BeforeClass
-    public void setup() {
-        ChromeOptions optionsChrome = new ChromeOptions();
-        optionsChrome.addArguments("--start-maximized");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(optionsChrome);
+    public WebDriver getDriver() {
+        this.driver = setDriver(DriverType.CHROME);
+        return driver;
     }
 
     @AfterClass
-    public void finish() {
-        if(driver != null) {
-            driver.quit();
-            driver = null;
-        }
+    public void tearDown() {
+        quitDriver();
     }
 
 }
--- src/test/java/testcases/IFrameTestCase.java
+++ src/test/java/testcases/IFrameTestCase.java
@@ -1,23 +1,28 @@
 package testcases;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import support.TestBase;
 import tasks.IFrameTasks;
 
 public class IFrameTestCase extends TestBase {
 
+    private WebDriver driver;
+    private IFrameTasks iFrameTasks;
     private static final String TXT_VALUE = "Your content goes here.";
+
+    @BeforeMethod
+    public void setUp() {
+        this.driver = getDriver();
+        driver.navigate().to("http://the-internet.herokuapp.com/iframe");
+        iFrameTasks = new IFrameTasks(driver);
+    }
 
     @Test
     public void iframeTest() {
-        driver.navigate().to("http://the-internet.herokuapp.com/iframe");
-
-        IFrameTasks iFrameTasks = new IFrameTasks(driver);
-
         Assert.assertEquals(iFrameTasks.returnIFrameHTMLTextfieldValue(), TXT_VALUE);
-
-        System.out.println(iFrameTasks.returnIFrameHTMLTextfieldValue());
+        System.out.println("Valor encontrado no iframe: " + iFrameTasks.returnIFrameHTMLTextfieldValue());
     }
 
 }
